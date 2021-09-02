74e46eff1b148130776152f6b1e825381daa31fe
Sebastião Neto <sebastiaomfneto@gmail.com>
2021-09-02 15:50:43 -0300

    Added palette configs on theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,17 @@
 import { createTheme } from '@material-ui/core';
+import { blue, cyan } from '@material-ui/core/colors';
 import { enUS } from '@material-ui/core/locale';
 
-export const theme = createTheme({}, enUS);
+export const theme = createTheme(
+  {
+    palette: {
+      primary: {
+        main: blue['500'],
+      },
+      secondary: {
+        main: cyan['A400'],
+      },
+    },
+  },
+  enUS
+);
